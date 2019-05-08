c8cf878a033aeb8f5c4f13ad6d0f83e7607709ac
Gerardo Anglada <gerangair@Gerardos-MacBook-Air.local>
2019-05-08 18:48:14 -0500

    Tracker for Students Society & reorganize

---
+++ sudo_noDB/cuenta.php
@@ -0,0 +1,97 @@
+
+<!-- -------------------------------------------------------------------------------
+ESTE EL LA PAGINA CUENTA USUARIO
+------------------------------------------------------------------------------- -->
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="/sudo_v2/assets/bootstrap.css">
+
+    <title>SUDO Tracker</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+  </head>
+  <body>
+    <!-- Navbar Component -->
+    <nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+      <a class="navbar-brand" href="main.php"><img src="/sudo_v2/assets/logo_sudo.png" height="30" alt="Logo SUDO">  SUDO Tracker</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="sudo_blog.html">Blog <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="cuenta.php">Cuenta</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="false">
+              Gestión Proyectos
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="proyectos.php">Solicitar otro proyecto</a>
+              <a class="dropdown-item" href="proyectos.php">Solicitar remoción de un proyecto</a>
+              <a class="dropdown-item" href="proyectos.php">Comentar sobre un proyecto</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <a class="navbar text-right lead">Hola Persona</a>
+    </nav>
+    <!-- End of navbar -->
+
+      <div class="jumbotron" style="background:url(/sudo_v2/assets/bg_it.png) no-repeat; background-size: cover;">
+        <div class="row px-4">
+          <div class="col-lg-4">
+            <div class="p-2 my-5 bg-secondary text-white text-left rounded">
+              <h1 class="display-4 ml-3">Modificar Cuenta</h1>
+              <p class="font-italic ml-4">Puedes cambiar contraseña y tu alias para que te digamos como te gusta!</p>
+            </div>
+          </div>
+        </div>
+    </div>
+    <div class="container px-5">
+      <div class="row">
+        <h3>Aqui irá la tablish con queries</h3>
+      </div> <!-- row2 -->
+    </div> <!-- container1 -->
+
+    <div class="row-fluid">
+      <div class="text-center bg-dark mt-5">
+        <ul class="nav justify-content-center">
+          <li class="nav-item">
+            <a class="nav-link active text-white" href="contactos_sudo.html">Contactos Mesa Directiva</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Reportar Errores</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="https://getbootstrap.com/docs/4.2/getting-started/introduction/" target="_blank"
+              rel="noopener noreferrer">Aprende Bootstrap</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+      crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+      integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+      crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+      integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+      crossorigin="anonymous"></script>
+  </body>
+</html>
