8949015ba71519bfa24ca443cdbcf74de1133ef0
GerAng1 <ganglada.dlanda@gmail.com>
2022-07-27 13:16:34 +0200

    Fixed sidebar & navigation of docs

--- projects/juego-la-aldea/aldeasite/village/urls_salem.py
+++ projects/juego-la-aldea/aldeasite/village/urls_salem.py
@@ -4,9 +4,10 @@ from . import views_salem
 app_name = 'salem'
 urlpatterns = [
     path('', views_salem.index, name='index'),
-    path('new-game/', views_salem.new_game, name='new-game'),
+    path('new-game', views_salem.new_game, name='new-game'),
     # path('edit/', views.edit, name='edit'),
     # path('recap/', views.recap, name='recap'),
-    path('docs/<str:main_script>', views_salem.docs, name='docs'),
-    path('docs/<str:object_model>/<int:object_pk>', views_salem.detail, name='detail'),
+    path('docs/', views_salem.rules, name='rules'),
+    path('docs/factions', views_salem.all_factions, name='factions'),
+    path('docs/details/<int:faction_pk>/', views_salem.detail, name='detail'),
 ]
--- projects/juego-la-aldea/aldeasite/village/views_salem.py
+++ projects/juego-la-aldea/aldeasite/village/views_salem.py
@@ -1,4 +1,3 @@
-from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
 from .models import Faction, Role, Theme
@@ -7,6 +6,7 @@ factions = Faction.objects.all()
 villagers = Role.objects.filter(faction=1)
 witches = Role.objects.filter(faction=2)
 lone_wolves = Role.objects.filter(faction=3)
+
 
 # Create your views here.
 def index(request):
@@ -22,45 +22,31 @@ def new_game(request):
         request, 'salem/new_game.html')
 
 
-def docs(request, main_script):
-    if main_script == 'rules':
-        return render(
-            request, 'salem/docs/rules.html',
-            {
-                'main_script': main_script,
-                'factions': factions,
-                'dict_roles': {1: villagers, 2: witches, 3: lone_wolves}})
-    else:
-        return render(
-            request, 'salem/docs/docs.html',
-            {
-                'main_script': main_script,
-                'factions': factions,
-                'dict_roles': {1: villagers, 2: witches, 3: lone_wolves}})
+def rules(request):
+    return render(
+        request, 'salem/docs/rules.html',
+        {
+            'main_script': "rules",
+            'factions': factions,
+            'dict_roles': {1: villagers, 2: witches, 3: lone_wolves}})
 
 
-def detail(request, object_model, object_pk):
-    if object_model == 'factions':
-        object = get_object_or_404(Faction, pk=object_pk)
+def all_factions(request):
+    return render(
+        request, 'salem/docs/factions.html',
+        {
+            'main_script': "factions",
+            'factions': factions,
+            'dict_roles': {1: villagers, 2: witches, 3: lone_wolves}})
 
-        return render(
-            request, 'salem/docs/faction.html',
-            {
-                'main_script': object_model,
-                'object': object,
-                'factions': factions,
-                'dict_roles': {1: villagers, 2: witches, 3: lone_wolves}})
 
-    elif object_model == 'roles':
-        object = get_object_or_404(Role, pk=object_pk)
+def detail(request, faction_pk):
+    current_faction = get_object_or_404(Faction, pk=faction_pk)
 
-        return render(
-            request, 'salem/docs/role.html',
-            {
-                'main_script': object_model,
-                'object': object,
-                'factions': factions,
-                'dict_roles': {1: villagers, 2: witches, 3: lone_wolves}})
-
-    else:
-        raise Http404("U did somethin wrong.")
+    return render(
+        request, 'salem/docs/detail.html',
+        {
+            'main_script': "factions",
+            'current_faction': current_faction,
+            'factions': factions,
+            'dict_roles': {1: villagers, 2: witches, 3: lone_wolves}})
